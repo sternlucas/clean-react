459af65a3d191250bfe3f1bd8f4dd42cee200935
sternlucas <contas.lucas@gmail.com>
2021-06-03 17:55:23 -0300

    refactor: move SUT creation to a factory mathod

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -4,7 +4,7 @@ export default class RemoteAuthentication {
   constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient,
-  ) {}
+  ) { }
 
   async auth(): Promise<void> {
     await this.httpPostClient.post(this.url);
