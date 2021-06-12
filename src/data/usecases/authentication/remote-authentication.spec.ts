1519be2a835220da6cb6cc778e8babad82005666
sternlucas <contas.lucas@gmail.com>
2021-06-12 13:57:42 -0300

    test: ensure RemoteAuthentication return an Account on success

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,11 +1,14 @@
 import faker from 'faker';
-import mockAuthentication from '@/domain/test/mock-authentication';
 import HttpPostClientSpy from '@/data/test/mock-http-client';
 import InvalidCredentialsError from '@/domain/errors/invalid-credentials-error';
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
 import UnexpectedError from '@/domain/errors/unexpected-error';
 import { AuthenticationParams } from '@/domain/usecases/authentication';
 import { AccountModel } from '@/domain/models/account-model';
+import {
+  mockAccountModel,
+  mockAuthentication,
+} from '@/domain/test/mock-account';
 import RemoteAuthentication from './remote-authentication';
 
 type SutTypes = {
@@ -88,14 +91,17 @@ describe('RemoteAuthentication', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 
-  test('Should throw UnexpectedError if HttpPostClient returns 500', async () => {
+  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
     const { sut, httpPostClientSpy } = makeSut();
 
+    const httpResult = mockAccountModel();
+
     httpPostClientSpy.response = {
-      statusCode: HttpStatusCode.serverError,
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
     };
 
-    const promise = sut.auth(mockAuthentication());
-    await expect(promise).rejects.toThrow(new UnexpectedError());
+    const account = await sut.auth(mockAuthentication());
+    expect(account).toEqual(httpResult);
   });
 });
--- src/domain/test/mock-authentication.ts
+++
@@ -1,9 +0,0 @@
-import faker from 'faker';
-import { AuthenticationParams } from '@/domain/usecases/authentication';
-
-const mockAuthentication = (): AuthenticationParams => ({
-  email: faker.internet.email(),
-  password: faker.internet.password(),
-});
-
-export default mockAuthentication;
