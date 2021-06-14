138e34814e0c937be8d6a7e77178d480597695a2
sternlucas <contas.lucas@gmail.com>
2021-06-14 07:44:10 -0300

    test: ensure AxiosHttpClient calls axios with corret verb

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -10,10 +10,10 @@ const makeSut = (): AxiosHttpClient => {
 };
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with corret URL', async () => {
+  test('Should call axios with corret URL and verb', async () => {
     const url = faker.internet.url();
     const sut = makeSut();
     await sut.post({ url });
-    expect(mockedAxios).toHaveBeenCalledWith(url);
+    expect(mockedAxios.post).toHaveBeenCalledWith(url);
   });
 });
