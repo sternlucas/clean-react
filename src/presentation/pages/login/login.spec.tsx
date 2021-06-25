5dac741aae9add869dfd91c15813d4439e47804c
sternlucas <contas.lucas@gmail.com>
2021-06-25 15:24:13 -0300

    refactor: change validation interface

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -16,10 +16,13 @@ type SutTypes = {
 class ValidationSpy implements Validation {
   errorMessage: string;
 
-  input: object;
+  fieldName: string;
 
-  validate(input: object): string {
-    this.input = input;
+  fieldValue: string;
+
+  validate(fieldName: string, fieldValue: string): string {
+    this.fieldName = fieldName;
+    this.fieldValue = fieldValue;
     return this.errorMessage;
   }
 }
@@ -58,9 +61,8 @@ describe('Login Component', () => {
     const emailInput = sut.getByTestId('email');
     fireEvent.input(emailInput, { target: { value: 'any_email' } });
 
-    expect(validationSpy.input).toEqual({
-      email: 'any_email',
-    });
+    expect(validationSpy.fieldName).toBe('email');
+    expect(validationSpy.fieldValue).toBe('any_email');
   });
 
   test('Should call Validation with correct password', () => {
@@ -68,8 +70,7 @@ describe('Login Component', () => {
     const passwordInput = sut.getByTestId('password');
     fireEvent.input(passwordInput, { target: { value: 'any_password' } });
 
-    expect(validationSpy.input).toEqual({
-      password: 'any_password',
-    });
+    expect(validationSpy.fieldName).toBe('password');
+    expect(validationSpy.fieldValue).toBe('any_password');
   });
 });
