3b02ce76b1b1d9fb3c58465d35a533e89ce0ca25
sternlucas <contas.lucas@gmail.com>
2021-08-19 22:10:14 -0300

    refactor: add tests for min value

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,16 +1,20 @@
 import { InvalidFieldError } from '@/validation/errors';
+import faker from 'faker';
 import { MinLengthValidation } from './min-length-validation';
+
+const makeSut = (): MinLengthValidation =>
+  new MinLengthValidation(faker.database.column(), 5);
 
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
-    const sut = new MinLengthValidation('field', 5);
-    const error = sut.validate('123');
+    const sut = makeSut();
+    const error = sut.validate(faker.random.alphaNumeric(4));
     expect(error).toEqual(new InvalidFieldError());
   });
 
   test('Should return error if value is valid', () => {
-    const sut = new MinLengthValidation('field', 5);
-    const error = sut.validate('123456');
+    const sut = makeSut();
+    const error = sut.validate(faker.random.alphaNumeric(5));
     expect(error).toBeFalsy();
   });
 });
