c8155dcc82c330e3c4932e55cbaa600345e0224c
sternlucas <contas.lucas@gmail.com>
2021-08-19 21:49:53 -0300

    feat: add tests for email

---
+++ src/validation/validators/email/email-validation.ts
@@ -0,0 +1,17 @@
+import { InvalidFieldError } from '@/validation/errors';
+import { FieldValidation } from '@/validation/protocols/field-validation';
+
+export class EmailValidation implements FieldValidation {
+  readonly field: string;
+
+  constructor(field: string) {
+    this.field = field;
+  }
+
+  validate(value: string): Error {
+    const emailRegex = new RegExp(
+      "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)",
+    );
+    return emailRegex.test(value) ? null : new InvalidFieldError();
+  }
+}
