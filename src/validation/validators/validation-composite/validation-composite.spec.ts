13b51e5920e8d0fdcbe5ee74e84d656449f6a0ba
sternlucas <contas.lucas@gmail.com>
2021-08-25 23:34:08 -0300

    refactor: move sut creation to a factory method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,16 +1,29 @@
 import { FieldValidationSpy } from '../test/mock-field-validation';
 import { ValidationComposite } from './validation-composite';
 
+type SutTypes = {
+  sut: ValidationComposite;
+  fieldValidationsSpy: FieldValidationSpy[];
+};
+
+const makeSut = (): SutTypes => {
+  const fieldValidationsSpy = [
+    new FieldValidationSpy(''),
+    new FieldValidationSpy(''),
+  ];
+  const sut = new ValidationComposite(fieldValidationsSpy);
+
+  return {
+    sut,
+    fieldValidationsSpy,
+  };
+};
+
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const fieldValidationSpy = new FieldValidationSpy('');
-    fieldValidationSpy.error = new Error('message');
-    const fieldValidationSpy2 = new FieldValidationSpy('');
-    fieldValidationSpy2.error = new Error('message');
-    const sut = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2,
-    ]);
+    const { sut, fieldValidationsSpy } = makeSut();
+    fieldValidationsSpy[0].error = new Error('message');
+    fieldValidationsSpy[1].error = new Error('message');
     const error = sut.validate('', '');
     expect(error).toBe('');
   });
