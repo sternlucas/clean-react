1494c61aeb5f845b6f61c2c49616194ac31784e0
sternlucas <contas.lucas@gmail.com>
2021-09-02 18:38:43 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/builder/validation-builder.spec.ts
+++ src/validation/builder/validation-builder.spec.ts
@@ -3,21 +3,40 @@ import {
   MinLengthValidation,
   RequiredFieldValidation,
 } from '@/validation/validators';
+import faker from 'faker';
 import { ValidationBuilder } from './validation-builder';
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const validations = ValidationBuilder.field('any_field').required().build();
-    expect(validations).toEqual([new RequiredFieldValidation('any_field')]);
+    const field = faker.database.column();
+    const validations = ValidationBuilder.field(field).required().build();
+    expect(validations).toEqual([new RequiredFieldValidation(field)]);
   });
 
   test('Should return EmailValidation', () => {
-    const validations = ValidationBuilder.field('any_field').email().build();
-    expect(validations).toEqual([new EmailValidation('any_field')]);
+    const field = faker.database.column();
+    const validations = ValidationBuilder.field(field).email().build();
+    expect(validations).toEqual([new EmailValidation(field)]);
   });
 
   test('Should return MinLenghtValidation', () => {
-    const validations = ValidationBuilder.field('any_field').min(5).build();
-    expect(validations).toEqual([new MinLengthValidation('any_field', 5)]);
+    const field = faker.database.column();
+    const length = faker.random.number();
+    const validations = ValidationBuilder.field(field).min(length).build();
+    expect(validations).toEqual([new MinLengthValidation(field, length)]);
+  });
+
+  test('Should return a list of validations', () => {
+    const field = faker.database.column();
+    const validations = ValidationBuilder.field(field)
+      .required()
+      .min(5)
+      .email()
+      .build();
+    expect(validations).toEqual([
+      new RequiredFieldValidation(field),
+      new MinLengthValidation(field, 5),
+      new EmailValidation(field),
+    ]);
   });
 });
