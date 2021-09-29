b3ca121480732a9e6a52bee333fef98b203a4a33
sternlucas <contas.lucas@gmail.com>
2021-09-29 08:11:01 -0300

    feat: add loagin composition

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -5,8 +5,8 @@ import {
 } from '@/data/protocols/http';
 import axios from 'axios';
 
-export class AxiosHttpClient implements HttpPostClient<unknown, unknown> {
-  async post(params: HttpPostParams<unknown>): Promise<HttpResponse<unknown>> {
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
     const httpResponse = await axios.post(params.url, params.body);
     return {
       statusCode: httpResponse.status,
--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,9 @@
 import ReactDOM from 'react-dom';
 import { Router } from '@/presentation/components';
 import '@/presentation/styles/global.scss';
+import { makeLogin } from './factories/pages/login/login-factory';
 
-ReactDOM.render(<Router />, document.getElementById('main'));
+ReactDOM.render(
+  <Router makeLogin={makeLogin} />,
+  document.getElementById('main'),
+);
