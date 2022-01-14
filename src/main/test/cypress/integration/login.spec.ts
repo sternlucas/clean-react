3c1609962d30ba0d428a9c8ccdad8a6715d8eaa9
sternlucas <contas.lucas@gmail.com>
2022-01-14 11:48:32 -0300

    test: ensure login present error message if invalid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,7 @@
 import faker from 'faker';
 
+const { baseUrl } = Cypress.config();
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login');
@@ -49,5 +51,22 @@ describe('Login', () => {
 
     cy.getByTestId('submit').should('not.have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email());
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5));
+
+    cy.getByTestId('submit').click();
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('contain.text', 'Credenciais invávalidas');
+    cy.url().should('eq', `${baseUrl}/login`);
   });
 });
