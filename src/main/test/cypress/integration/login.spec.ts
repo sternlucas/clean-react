0a3aa27a66686169b66f2480e5cb4d6b24069de7
sternlucas <contas.lucas@gmail.com>
2022-02-09 14:04:26 -0300

    test: prevent Login to call submit multiple times

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -134,4 +134,20 @@ describe('Login', () => {
     );
     cy.url().should('eq', `${baseUrl}/login`);
   });
+
+  it('Should present multiplesubmits', () => {
+    cy.intercept('POST', /login/, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.datatype.uuid(),
+      },
+    }).as('request');
+
+    cy.getByTestId('email').focus().type(faker.internet.email());
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5));
+
+    cy.getByTestId('submit').dblclick();
+
+    cy.get('@request.all').should('have.length', 1);
+  });
 });
